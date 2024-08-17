1187427cd0db204a856e2299b63f9483dcf8e794
Chetan Kolhe <Chetankolhe1020@gmail.com>
2024-08-17 14:00:52 +0530

    wsws

---
+++ addto_db_byusername.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+$conn1 = mysqli_connect("localhost","root","","user_cart")or die(mysqli_error());
+if(isset($_SESSION['userpass']))
+{
+
+    $user_pass=$_SESSION['userpass'];
+$sql = "CREATE TABLE `$user_pass` (
+    `itemid` INT NOT NULL,
+    `userpass` VARCHAR(232) NOT NULL,
+    `name` VARCHAR(232) NOT NULL,
+    `price` VARCHAR(232) NOT NULL,
+    `storage` VARCHAR(232) NOT NULL,
+    `image` VARCHAR(2323) NOT NULL,
+    `quantity` INT NOT NULL,
+    PRIMARY KEY (`itemid`)
+)";
+$result = mysqli_query($conn1 , $sql);
+// if($result){
+   
+//     header('location:home1.php');
+   
+// }
+// else{
+//    echo "no";
+//    header('location:home1.php');
+// }
+
+}
+?>
